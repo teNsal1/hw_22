d6c57a53c6a1795d9d63534b2de209485c9e3e1e
Багаутдинов-Ринат <tensal@bk.ru>
2025-07-30 11:52:56 +0300

    hw_22: Настраиваем контекстный процессор

--- barbershop_project/barbershop/settings.py
+++ barbershop_project/barbershop/settings.py
@@ -40,6 +40,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'core.context_processors.navbar',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
